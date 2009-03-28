0ad8367359a1bdd38eadee38882902bb3f426884
David Dalrymple <me@gimel.mit.edu>
2009-03-28 17:20:43 -0400

    Complete glyph set

--- ala_glyphs.c
+++ ala_glyphs.c
@@ -1,15 +1,17 @@
 #include "ala_glyphs.h"
 
 void xor_gate_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_translate(cr,0.04,0);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.35, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 0.8, 1.0);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.35, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.68, 0.68, 1.0);
+	}
 
 	cairo_new_path(cr);
 	cairo_move_to(cr,0,0);
@@ -24,7 +26,6 @@ void xor_gate_glyph(cairo_t *cr) {
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -43,14 +44,16 @@ void xor_gate_glyph(cairo_t *cr) {
 }
 
 void or_gate_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.35, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 1.0, 0.8);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.35, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.68, 1.0, 0.68);
+	}
 
 	cairo_new_path(cr);
 	cairo_move_to(cr,0,0);
@@ -65,7 +68,6 @@ void or_gate_glyph(cairo_t *cr) {
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -78,14 +80,16 @@ void or_gate_glyph(cairo_t *cr) {
 }
 
 void and_gate_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 1.0, 0.7, 0.7);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 1.0, 0.68, 0.68);
+	}
 
 	cairo_new_path(cr);
 	cairo_move_to(cr,0.0,0.0);
@@ -95,7 +99,6 @@ void and_gate_glyph(cairo_t *cr) {
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -108,28 +111,29 @@ void and_gate_glyph(cairo_t *cr) {
 }
 
 void nand_gate_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
-	cairo_scale(cr,0.75,0.8);
+	cairo_scale(cr,0.68,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 1.0, 0.96, 0.7);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 1.0, 1.0, 0.8);
+	}
 
 	cairo_new_path(cr);
 	cairo_move_to(cr,0.0,0.0);
 	cairo_line_to(cr,0.0,1.0);
 	cairo_line_to(cr,0.5,1.0);
 	cairo_arc_negative(cr,0.5,0.5,0.5,M_PI/2.0,0);
-        cairo_arc(cr,1.08,0.5,0.08,M_PI,0);
-        cairo_arc(cr,1.08,0.5,0.08,0,M_PI);
+        cairo_arc(cr,1.1,0.5,0.1,M_PI,0);
+        cairo_arc(cr,1.1,0.5,0.1,0,M_PI);
 	cairo_arc_negative(cr,0.5,0.5,0.5,0,-M_PI/2.0);
 
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -142,14 +146,16 @@ void nand_gate_glyph(cairo_t *cr) {
 }
 
 void stem_cell_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 0.8, 0.8);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 0.8, 0.8);
+	}
 
 	cairo_new_path(cr);
 	cairo_move_to(cr,0.0,0.0);
@@ -159,7 +165,6 @@ void stem_cell_glyph(cairo_t *cr) {
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -172,24 +177,29 @@ void stem_cell_glyph(cairo_t *cr) {
 }
 
 void crossover_cell_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 0.5, 0.8);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.8, 0.5, 0.8);
+	}
 
 	cairo_new_path(cr);
+#ifdef CROSSOVER_DIAMOND
 	cairo_move_to(cr,0.5,0.0);
 	cairo_line_to(cr,0.0,0.5);
 	cairo_line_to(cr,0.5,1.0);
 	cairo_line_to(cr,1.0,0.5);
+#else
+	cairo_arc(cr,0.5,0.5,0.5,0,M_PI*2);
+#endif
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -197,26 +207,35 @@ void crossover_cell_glyph(cairo_t *cr) {
 	cairo_set_line_width(cr,0.07);
 	cairo_set_line_join(cr,CAIRO_LINE_JOIN_ROUND);
 	cairo_stroke(cr);
+
+#ifndef CROSSOVER_DIAMOND
+	cairo_move_to(cr,0.5+sqrt(2.0)/4.0,0.5+sqrt(2.0)/4.0);
+	cairo_line_to(cr,0.5-sqrt(2.0)/4.0,0.5-sqrt(2.0)/4.0);
+	cairo_move_to(cr,0.5-sqrt(2.0)/4.0,0.5+sqrt(2.0)/4.0);
+	cairo_line_to(cr,0.5+sqrt(2.0)/4.0,0.5-sqrt(2.0)/4.0);
+	cairo_stroke(cr);
+#endif
 	
 	cairo_restore(cr);
 }
 
 void wire_cell_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.9, 0.7, 0.4);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.9, 0.7, 0.4);
+	}
 
 	cairo_new_path(cr);
 	cairo_arc(cr,0.5,0.5,0.5,0,M_PI*2);
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -228,23 +247,24 @@ void wire_cell_glyph(cairo_t *cr) {
 	cairo_restore(cr);
 }
 
-void delete_cell_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+void delete_cell_glyph(cairo_t *cr, int rotation) {
+	static cairo_pattern_t *pat = 0;
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.9, 0.3, 0.2);
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.9, 0.3, 0.2);
+	}
 
 	cairo_new_path(cr);
-	cairo_arc(cr,0.5,0.5,0.5,-3*M_PI/4,3*M_PI/4);
+	cairo_arc(cr,0.5,0.5,0.5,(rotation*2-3)*M_PI/4,(rotation*2+3)*M_PI/4);
 	cairo_line_to(cr,0.5,0.5);
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -256,23 +276,24 @@ void delete_cell_glyph(cairo_t *cr) {
 	cairo_restore(cr);
 }
 
-void copy_cell_glyph(cairo_t *cr) {
-	cairo_pattern_t *pat;
+void copy_cell_glyph(cairo_t *cr, int rotation) {
+	static cairo_pattern_t *pat = 0;
+
+	if(pat == 0) {
+		pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
+		cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
+		cairo_pattern_add_color_stop_rgb (pat, 1, 0.2, 0.9, 0.3);
+	}
 
 	cairo_save(cr);
 	cairo_scale(cr,0.8,0.8);
 
-	pat = cairo_pattern_create_radial (0.27, 0.3, 0.08, 0.2, 0.35, 0.6);
-	cairo_pattern_add_color_stop_rgb (pat, 0, 0.9, 0.9, 0.9);
-	cairo_pattern_add_color_stop_rgb (pat, 1, 0.2, 0.9, 0.3);
-
 	cairo_new_path(cr);
-	cairo_arc(cr,0.5,0.5,0.5,-5*M_PI/4,5*M_PI/4);
+	cairo_arc(cr,0.5,0.5,0.5,(rotation*2-5)*M_PI/4,(rotation*2+5)*M_PI/4);
 	cairo_line_to(cr,0.5,0.5);
 	cairo_close_path(cr);
 
 	cairo_set_source (cr, pat);
-	cairo_pattern_destroy (pat);
 
 	cairo_fill_preserve (cr);
 
@@ -281,5 +302,153 @@ void copy_cell_glyph(cairo_t *cr) {
 	cairo_set_line_join(cr,CAIRO_LINE_JOIN_ROUND);
 	cairo_stroke(cr);
 	
+	cairo_restore(cr);
+}
+
+void arrow_none_glyph(cairo_t *cr) {
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_POS_X,ARROW_POS_Y);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,ARROW_BODY_START,0.25-ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_BODY_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_END,0.25);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_BODY_RADIUS);
+	cairo_close_path(cr);
+	
+	cairo_set_source_rgb(cr,0.8,0.8,0.8);
+	cairo_fill(cr);
+
+	cairo_restore(cr);
+}
+
+void arrow_x_glyph(cairo_t *cr) {
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_POS_X,ARROW_POS_Y);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,ARROW_BODY_START,0.25-ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_BODY_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_END,0.25);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_BODY_RADIUS);
+	cairo_close_path(cr);
+	
+	cairo_set_source_rgb(cr,0.0,0.0,0.0);
+	cairo_fill(cr);
+
+	cairo_restore(cr);
+}
+
+void arrow_0_glyph(cairo_t *cr) {
+	static cairo_pattern_t *pat_in, *pat_out = 0;
+
+	if(pat_in == 0) {
+		pat_in = cairo_pattern_create_radial (ARROW_HEAD_START-0.2, 0.25, 0.01, ARROW_HEAD_START-0.2, 0.25, 1.0);
+		cairo_pattern_add_color_stop_rgb (pat_in, 0, 0.88, 0.9, 1.0);
+		cairo_pattern_add_color_stop_rgb (pat_in, ARROW_BODY_RADIUS*(0.7), 0.35, 0.35, 1.0);
+		cairo_pattern_add_color_stop_rgb (pat_in, 1, 0.0, 0.0, 0.7);
+	}
+
+	if(pat_out == 0) {
+		pat_out = cairo_pattern_create_radial (0, 0, ARROW_BODY_RADIUS*(0.7), 0.03, 0, 0.3);
+		cairo_pattern_add_color_stop_rgba(pat_out, 0, 0.3, 0.3, 1.0, 0.6);
+		cairo_pattern_add_color_stop_rgba(pat_out, 1, 0.1, 0.1, 1.0, 0.0);
+	}
+
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_POS_X,ARROW_POS_Y);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,0.0,0.0);
+	cairo_line_to(cr,0.0,0.5);
+	cairo_line_to(cr,1.0,0.5);
+	cairo_line_to(cr,1.0,0.0);
+	cairo_close_path(cr);
+
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_HEAD_START-0.2, 0.25);
+	cairo_scale(cr,1.7,0.9);
+	cairo_set_source(cr,pat_out);
+	cairo_fill(cr);
+	cairo_restore(cr);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,ARROW_BODY_START,0.25-ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_BODY_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_END,0.25);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_BODY_RADIUS);
+	cairo_close_path(cr);
+	
+	cairo_set_source(cr,pat_in);
+	cairo_fill_preserve(cr);
+
+	cairo_set_source_rgb(cr,0,0,0);
+	cairo_set_line_width(cr,0.03);
+	cairo_clip_preserve(cr);
+	cairo_stroke(cr);
+
+	cairo_restore(cr);
+}
+
+void arrow_1_glyph(cairo_t *cr) {
+	static cairo_pattern_t *pat_in, *pat_out = 0;
+
+	if(pat_in == 0) {
+		pat_in = cairo_pattern_create_radial (ARROW_HEAD_START-0.1, 0.25, 0.01, ARROW_HEAD_START-0.1, 0.25, 1.0);
+		cairo_pattern_add_color_stop_rgb (pat_in, 0, 1.0, 0.9, 0.88);
+		cairo_pattern_add_color_stop_rgb (pat_in, ARROW_BODY_RADIUS*(0.7), 1.0, 0.35, 0.35);
+		cairo_pattern_add_color_stop_rgb (pat_in, 1, 0.7, 0.0, 0.0);
+	}
+
+	if(pat_out == 0) {
+		pat_out = cairo_pattern_create_radial (0, 0, ARROW_BODY_RADIUS, 0.03, 0, 0.3);
+		cairo_pattern_add_color_stop_rgba(pat_out, 0, 1.0, 0.3, 0.3, 0.6);
+		cairo_pattern_add_color_stop_rgba(pat_out, 1, 1.0, 0.1, 0.1, 0.0);
+	}
+
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_POS_X,ARROW_POS_Y);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,0.0,0.0);
+	cairo_line_to(cr,0.0,0.5);
+	cairo_line_to(cr,1.0,0.5);
+	cairo_line_to(cr,1.0,0.0);
+	cairo_close_path(cr);
+
+	cairo_save(cr);
+	cairo_translate(cr,ARROW_HEAD_START-0.1, 0.25);
+	cairo_scale(cr,1.7,0.9);
+	cairo_set_source(cr,pat_out);
+	cairo_fill(cr);
+	cairo_restore(cr);
+
+	cairo_new_path(cr);
+	cairo_move_to(cr,ARROW_BODY_START,0.25-ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_BODY_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_BODY_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25+ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_END,0.25);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_HEAD_RADIUS);
+	cairo_line_to(cr,ARROW_HEAD_START,0.25-ARROW_BODY_RADIUS);
+	cairo_close_path(cr);
+	
+	cairo_set_source(cr,pat_in);
+	cairo_fill_preserve(cr);
+
+	cairo_set_source_rgb(cr,0,0,0);
+	cairo_set_line_width(cr,0.03);
+	cairo_clip_preserve(cr);
+	cairo_stroke(cr);
+
 	cairo_restore(cr);
 }
--- ala_glyphs_test.c
+++ ala_glyphs_test.c
@@ -6,11 +6,11 @@ main (int argc, char *argv[])
         cairo_surface_t *surface;
         cairo_t *cr;
 
-        surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 750, 300);
+        surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 500, 250);
         cr = cairo_create (surface);
 	/* Examples are in 1.0 x 1.0 coordinate space */
-	cairo_translate(cr, 15,15);
-	cairo_scale (cr, 150, 150);
+	cairo_translate(cr, 10,10);
+	cairo_scale (cr, 100, 100);
 
 	/* Drawing code goes here */
 	and_gate_glyph(cr);
@@ -27,9 +27,17 @@ main (int argc, char *argv[])
 	cairo_translate(cr,-1.0,0);
 	wire_cell_glyph(cr);
 	cairo_translate(cr,-1.0,0);
-	delete_cell_glyph(cr);
+	delete_cell_glyph(cr,3);
 	cairo_translate(cr,-1.0,0);
-	copy_cell_glyph(cr);
+	copy_cell_glyph(cr,3);
+	cairo_translate(cr,0,1.0);
+	arrow_none_glyph(cr);
+	cairo_translate(cr,1.0,0);
+	arrow_x_glyph(cr);
+	cairo_translate(cr,1.0,0);
+	arrow_0_glyph(cr);
+	cairo_translate(cr,1.0,0);
+	arrow_1_glyph(cr);
 
 	/* Write output and clean up */
         cairo_surface_write_to_png (surface, "ala_glyphs.png");
