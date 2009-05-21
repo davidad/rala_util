f2647d6a24fb720f0c1034860c17fbc23c81d2cc
David Dalrymple <davidad@dalet.(none)>
2009-05-21 19:46:46 -0400

    other misc files removed

--- rala_gylph_cb.h
+++
@@ -1,4 +0,0 @@
-#include "commands.h"
-
-void set_cell_cb (void* v_cr, int x, int y, cell_type_t cell_type);
-void set_arrow_cb(void* v_cr, int x, int y, arrow_dir_t arrow_dir, arrow_type_t arrow_type);
